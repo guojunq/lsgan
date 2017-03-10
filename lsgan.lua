f6f83aeb24c10b0850b1cc3da3416e0e00c815e8
guojunq <guojunq@gmail.com>
2017-03-09 23:30:40 -0500

    Update lsgan.lua

--- lsgan.lua
+++ lsgan.lua
@@ -30,8 +30,8 @@ opt = {
    gpu = 1,                -- gpu = -1 is CPU mode. gpu=X is GPU mode on GPU X
    name = 'lsgan_result',
    noise = 'uniform',       -- uniform / normal
-   lambda=0.008,              -- the scale of the distance metric used for adaptive margins. This is actually tau in the original paper. L2: 0.05/L1: 0.001, temporary best 0.008 before applying scaling, 
-   gamma = 0.,		      -- the coefficient for loss minimization term.  Set to zero for non-conditional LS-GAN as the theorem shows this term can be ignored. 
+   lambda=0.0002,              -- the scale of the distance metric used for adaptive margins. This is actually tau in the original paper. L2: 0.05/L1: 0.001, temporary best 0.008 before applying scaling, 
+   gamma = 0.1,		      -- the coefficient for loss minimization term.  Set to zero for non-conditional LS-GAN as the theorem shows this term can be ignored. But setting it to a small value allows a more stable result.
    decay_rate = 0.00,  -- weight decay: 0.00005 
 }
 
